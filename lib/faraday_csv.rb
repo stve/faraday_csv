f587a63293b6a0629c325523a8ec4f2f7e51c50f
Steve Agalloco <steve.agalloco@gmail.com>
2013-09-28 23:53:41 -0400

    register the middleware

--- lib/faraday_csv.rb
+++ lib/faraday_csv.rb
@@ -2,4 +2,9 @@ require 'faraday'
 
 class Faraday::Response
   autoload :CSV, 'faraday/response/csv.rb'
+
+  if Faraday.respond_to? :register_middleware
+    Faraday.register_middleware :response,
+      :csv => lambda { Faraday::Response::CSV }
+  end
 end
